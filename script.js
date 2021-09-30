7306d73a055e39bd874e5ba1a7667b7d3cb007dd
Margaret <mrobinson526@gmail.com>
2021-09-30 12:20:14 -0500

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 var accusation = 1;
-var friendArr = ["Tony", "James", "Mary", "John", "Sheila"];
-var weaponArr = ["shovel", "axe", "knife", "bat", "cup", "plate", "chair", "brick", "coin", "shoe", "purse", "fruit", "phone", "pen", "vase", "cd", "tape", "broom", "cup", "keys"];
-var locationArr = ["copy room", "kitchen", "back yard", "front yard", "porch", "swimming pool", "field", "break room", "class room", "den"];
-
-
 var createH3 = function() {
   var h3 = $('<h3></h3>').text("Accusation " + accusation);
   $('body').append(h3);
@@ -11,6 +6,9 @@ var createH3 = function() {
 
   if (accusation > 100) {
     console.log("Reached 100;");
+    $(".container>h3").click(function() {
+      alert("Accusation" [i] + ": " + "I accuse " + friendArr[i] + ", " + "with the " + weaponArr[i] + " in the " + locationArr[i] + "!" );
+    })
     return;
   } else {
     createH3();
@@ -19,11 +17,23 @@ var createH3 = function() {
 
 $(document).ready(function() {
   createH3();
-  $("h3").on("click", function(msgH3) {
-    alert("Accusation" + accusation + ":" + " I accuse " + friendArr[i] + " with the " + weaponArr[i] + " in the " + locationArr[i] + "!");
-  })
-msgH3();
-  for (i=0; i<99999; i++) {
-    console.log(friendArr[i%friendArr.length], weaponArr[i%weaponArr.length], locationArr[i%locationArr.length]);
+  });
+  
+  function makeArr() {
+    var accusation = 1;
+    var friendArr = ["Tony", "James", "Mary", "John", "Sheila"];
+    var weaponArr = ["shovel", "axe", "knife", "bat", "cup", "plate", "chair", "brick", "coin", "shoe", "purse", "fruit", "phone", "pen", "vase", "cd", "tape", "broom", "cup", "keys"];
+    var locationArr = ["copy room", "kitchen", "back yard", "front yard", "porch", "swimming pool", "field", "break room", "class room", "den"];
+  };
+  function displayArr() {
+    for (i=0; i<Math.max(friendArr.length, weaponArr.length, locationArr.length); i++) {
+      var arrFri = friendArr[i%friendArr.length];
+      var arrWea = weaponArr[i%weaponArr.length];
+      var arrLoc = locationArr(i%locationArr.length);
+      alert("Accusation" [i] + ": " + "I accuse " + friendArr[i] + ", " + "with the " + weaponArr[i] + " in the " + locationArr[i] + "!" );
+    }
+    return displayArr;
   }
-})
+  var myArr = makeArr();
+  myArr();
+  
